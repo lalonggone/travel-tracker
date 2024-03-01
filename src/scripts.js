90dced9d545c5151e5b52949fdd984b6348830c9
Laura Long <lauralonggone@gmail.com>
2024-03-01 12:25:59 -0700

    Refactor API calls and create categorizeTrips function

--- src/scripts.js
+++ src/scripts.js
@@ -8,3 +8,24 @@ import './images/turing-logo.png'
 import { getAllData } from "./apiCalls";
 
 getAllData()
+
+
+
+function categorizeTrips(trips, currentDate) {
+    const pastTrips = [];
+    const upcomingTrips = [];
+    const pendingTrips = [];
+
+    trips.forEach(trip => {
+        const tripDate = new Date(trip.date);
+        if (tripDate < currentDate) {
+            pastTrips.push(trip);
+        } else if (tripDate > currentDate) {
+            upcomingTrips.push(trip);
+        } else {
+            pendingTrips.push(trip);
+        }
+    });
+
+    return { pastTrips, upcomingTrips, pendingTrips };
+}
